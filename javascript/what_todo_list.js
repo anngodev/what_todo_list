19ac9105cd051f2e81e2f6c0c57905798181558e
Andy <ngoan_230@yahoo.com>
2020-03-14 16:01:31 -0400

    Connected ul and li to be sortable

--- javascript/what_todo_list.js
+++ javascript/what_todo_list.js
@@ -1,1 +1,12 @@
 // WHAT TODO LIST JS
+
+$("todoList").sortable({
+    items: "li:not('.listTitle, .addItem')",
+    connectWith: "ul",
+    dropOnEmpty: true,
+    placeholder: "emptySpace"
+});
+
+$("input").keydown(function() {
+    
+});
